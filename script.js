8334727fb6c308244596d89f64f95cba2d729aec
Apertureon <taianchen.chn@gmail.com>
2024-05-29 22:45:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const grid = document.querySelector('.gallery');
     const msnry = new Masonry(grid, {
         itemSelector: '.photo',
-        columnWidth: 200,
+        columnWidth: 300,
         gutter: 10
     });
 
@@ -12,21 +12,25 @@ document.addEventListener('DOMContentLoaded', function() {
         img.onload = function() {
             const photoDiv = document.createElement('div');
             photoDiv.className = 'photo';
-            grid.appendChild(photoDiv);
+
+            const detailsDiv = document.createElement('div'); // Create the details div here
+            detailsDiv.className = 'details'; // Assign class name
+
             photoDiv.appendChild(img);
+            photoDiv.appendChild(detailsDiv); // Append details div to photoDiv
+            grid.appendChild(photoDiv);
 
             EXIF.getData(img, function() {
                 var aperture = EXIF.getTag(this, 'FNumber');
                 var shutterSpeed = EXIF.getTag(this, 'ExposureTime');
                 var iso = EXIF.getTag(this, 'ISOSpeedRatings');
 
-                var details = img.nextElementSibling; // The details div
                 if (aperture || shutterSpeed || iso) {
-                    details.innerHTML = `Aperture: f/${aperture ? aperture.numerator / aperture.denominator : 'N/A'} | ` +
-                                        `Shutter Speed: ${shutterSpeed ? shutterSpeed.numerator + '/' + shutterSpeed.denominator + ' sec' : 'N/A'} | ` +
-                                        `ISO: ${iso || 'N/A'}`;
+                    detailsDiv.innerHTML = `Aperture: f/${aperture ? aperture.numerator / aperture.denominator : 'N/A'} | ` +
+                                           `Shutter Speed: ${shutterSpeed ? shutterSpeed.numerator + '/' + shutterSpeed.denominator + ' sec' : 'N/A'} | ` +
+                                           `ISO: ${iso || 'N/A'}`;
                 } else {
-                    details.innerHTML = "No EXIF Data found.";
+                    detailsDiv.innerHTML = "No EXIF Data found.";
                 }
             });
 
