fd2826e30953f40775f676016911cbe170ddcd30
Apertureon <taianchen.chn@gmail.com>
2024-05-29 20:59:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const gallery = document.querySelector('#gallery');
-    let rowImages = [];
-    let totalAspectRatio = 0;
-    const maxRowWidth = gallery.clientWidth; // Maximum row width
+    const grid = document.querySelector('.gallery');
+    const msnry = new Masonry(grid, {
+        itemSelector: '.photo',
+        columnWidth: 200,
+        gutter: 10
+    });
 
-    imageList.forEach((src, index) => {
+    imageList.forEach(src => {
         const img = new Image();
         img.src = src;
         img.onload = function() {
-            const aspectRatio = this.width / this.height;
-            totalAspectRatio += aspectRatio;
-            rowImages.push({img, aspectRatio});
-
-            if (totalAspectRatio * 120 > maxRowWidth) { // 120 is an example row height to start with
-                adjustRowHeight(rowImages, totalAspectRatio, maxRowWidth);
-                rowImages = [];
-                totalAspectRatio = 0;
-            }
-
             const photoDiv = document.createElement('div');
             photoDiv.className = 'photo';
+            grid.appendChild(photoDiv);
             photoDiv.appendChild(img);
-            const detailsDiv = document.createElement('div');
-            detailsDiv.className = 'details';
-            detailsDiv.innerHTML = `Aperture: f/${img.aperture || 'N/A'} | Shutter Speed: ${img.shutterSpeed || 'N/A'} sec | ISO: ${img.iso || 'N/A'}`;
-            photoDiv.appendChild(detailsDiv);
-            gallery.appendChild(photoDiv);
+
+            EXIF.getData(img, function() {
+                const aperture = EXIF.getTag(this, 'ApertureValue');
+                const shutterSpeed = EXIF.getTag(this, 'ShutterSpeedValue');
+                const iso = EXIF.getTag(this, 'ISOSpeedRatings');
+                const detailsDiv = document.createElement('div');
+                detailsDiv.className = 'details';
+                detailsDiv.textContent = `Aperture: f/${aperture || 'N/A'} | Shutter Speed: ${shutterSpeed || 'N/A'} sec | ISO: ${iso || 'N/A'}`;
+                photoDiv.appendChild(detailsDiv);
+            });
+
+            msnry.appended(photoDiv);
+            msnry.layout();
         };
-
-        // Ensure onload fires for cached images
-        if (img.complete) {
-            img.onload();
-        }
     });
-
-    function adjustRowHeight(images, totalAspectRatio, maxWidth) {
-        const optimalHeight = maxWidth / totalAspectRatio;
-        images.forEach(({img, aspectRatio}) => {
-            img.style.width = `${optimalHeight * aspectRatio}px`;
-            img.style.height = `${optimalHeight}px`;
-        });
-    }
 });
