c971d752be751a0fef85c39f6b9f252e24ddaac3
Apertureon <taianchen.chn@gmail.com>
2024-06-01 00:44:23 +0100

    Delete imageList.js

--- imageList.js
+++
@@ -1,1 +0,0 @@
-const imageList = ["images/IMG_0001.jpg","images/IMG_0002.jpg","images/IMG_0003.jpg","images/IMG_0004.jpg","images/IMG_0005.jpg","images/IMG_0006.jpg","images/IMG_0007.jpg","images/IMG_0008.jpg","images/IMG_0009.jpg","images/IMG_0010.jpg","images/IMG_0011.jpg","images/IMG_0012.jpg"];
